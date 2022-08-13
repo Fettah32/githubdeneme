8eb1c1d4b6ca476b25d61b5963cc79ea587086f0
Adil Fettah Yassıkaya <adilyassikaya@gmail.com>
2022-08-13 21:55:02 +0300

    2.C

--- src/Day01_Git.java
+++ src/Day01_Git.java
@@ -3,5 +3,6 @@ public class Day01_Git {
     public static void main(String[] args) {
 
         System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 2. Deneme");
     }
 }
