f15082d27ef912374583186f72c4d3baabc460de
Adil Fettah Yassıkaya <adilyassikaya@gmail.com>
2022-08-15 23:14:55 +0300

    B5

--- src/Day02_GitGithub.java
+++ src/Day02_GitGithub.java
@@ -24,5 +24,6 @@ public class Day02_GitGithub {
     public static void main(String[] args) {
         System.out.println("Branch 1. deneme");
         System.out.println("Master 1. deneme");
+        System.out.println("Branch 2. deneme");
     }
 }
