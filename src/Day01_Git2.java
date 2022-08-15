3ecbde334e2033a1e3a095853a67e6fd1a9d4caa
Adil Fettah Yassıkaya <adilyassikaya@gmail.com>
2022-08-15 21:03:47 +0300

    V1

---
+++ src/Day01_Git2.java
@@ -0,0 +1,24 @@
+public class Day01_Git2 {
+    public static void main(String[] args) {
+        System.out.println("Git 2. Class");
+
+        /*
+        --------Komutlar
+    git --version
+    git config --global user.email "email_adresiniz"
+    git config --global user.name "isminiz"
+    git config --global -l
+    git init
+    git status
+    git add .
+    git status
+    git diff --> Working space deki değişikliği gösterir
+    git diff --staged --> Staging Area daki değişiklikleri gösterir
+    git commit -m "first commit"
+    git show "hashcode" --> Versiyondaki değişiklikleri gösterir
+    git log --oneline
+    git remote add origin https://github.com/techproed2020/Git_...
+    git push -u origin master
+         */
+    }
+}
---
+++ src/Day02_GitGithub.java
@@ -0,0 +1,21 @@
+public class Day02_GitGithub {
+    /*
+    1-) git init --> Local repo olusturmak icin yani .git ile klasorumuzun
+    icindeki dosyalari iliskilendirmek icin kullanilir
+
+    2-) git add . --> Working space'den (yani yerel) dosyalarimi staging area'ya (yani commitlemek icin beklenen yer)
+    gonderir
+
+    3-) git status --> git status --> Working space'deki ve staging area'daki durumu gosterir
+
+    4-) git commit -m "mesaj" --> Staging area'dan commit store dosyalarimi gondermek icin kullanilir
+    (commit yani versiyon yani surum olusturmus olurum)
+
+    5-) git push --> Uzak repo'ya (yani remote - Github) gondermek icin kullandigimiz kod
+    Yalniz git push komutunu direk kullanmak istersek bir kez
+                git remote add origin -adress-
+                git push -u origin master
+    NOT : Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci commit'lerim icin
+    sadece git push kullaniriz
+     */
+}
